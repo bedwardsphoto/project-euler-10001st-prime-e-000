439ebcc6a1b780832ed62c24c2e6fc579dbb9228
bedwardsphoto <brian@brianedwardsphotography.com>
2016-01-31 20:57:20 -0500

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,14 @@
-# Implement your procedural solution here!
+require 'prime'
+
+def prime_number_for(nth_element)
+  x=0
+  primes=0
+  until x==nth_element.to_i
+    primes+=1
+    if primes.prime?
+      x+=1
+    end
+  end
+  return primes
+end
+
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,25 @@
-# Implement your object-oriented solution here!
+require 'prime'
+class Prime
+def initialize(input)
+  @input=input
+end
+def input=(input)
+  end
+def input
+  @input
+end
+
+def number
+  x=0
+  primes=0
+  until x==@input.to_i
+    primes+=1
+    if primes.prime?
+      x+=1
+    end
+  end
+  return primes
+end
+
+
+  end
